00fbc48df65dd4bb25b1d4c88fe11e96ff3b9619
Javed Ansari <Javedansari2k09@gmail.com>
2026-01-01 23:07:59 +0530

    all setup config

--- src/app.js
+++ src/app.js
@@ -13,4 +13,13 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(cookieParser());
 app.use(express.static("public"));
 
+
+// import Routes
+import userRouter from "./routes/user.router.js";
+
+
+// Route declaration
+app.use("/api/v1/users", userRouter);
+
+
 export {app}
